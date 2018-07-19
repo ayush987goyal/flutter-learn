896198e9c5429ef0ff087b96f305162127cc15e7
Ayush Goyal <ayush987goyal@gmail.com>
2018-07-19 20:43:24 +0530

    Add fields to auth page and make it home

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -1,18 +1,58 @@
 import 'package:flutter/material.dart';
 
-class AuthPage extends StatelessWidget {
+class AuthPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _AuthPageState();
+  }
+}
+
+class _AuthPageState extends State<AuthPage> {
+  String _emailValue;
+  String _passwordValue;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Login'),
       ),
-      body: Center(
-        child: RaisedButton(
-          child: Text('Login'),
-          onPressed: () {
-            Navigator.pushReplacementNamed(context, '/');
-          },
+      body: Container(
+        margin: EdgeInsets.all(10.0),
+        child: ListView(
+          children: <Widget>[
+            TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(labelText: 'E-Mail'),
+              onChanged: (String value) {
+                setState(() {
+                  _emailValue = value;
+                });
+              },
+            ),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(labelText: 'Password'),
+              onChanged: (String value) {
+                setState(() {
+                  _passwordValue = value;
+                });
+              },
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            RaisedButton(
+              color: Theme.of(context).primaryColor,
+              textColor: Colors.white,
+              child: Text('Login'),
+              onPressed: () {
+                print(_emailValue);
+                print(_passwordValue);
+                Navigator.pushReplacementNamed(context, '/products');
+              },
+            ),
+          ],
         ),
       ),
     );
