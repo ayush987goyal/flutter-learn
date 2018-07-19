77b33b6ff687971bc7de9ac0bcf750e02ac481a0
Ayush Goyal <ayush987goyal@gmail.com>
2018-07-19 20:22:04 +0530

    Refactor the add delete product methods and remove prod control

--- lib/pages/products_admin.dart
+++ lib/pages/products_admin.dart
@@ -4,6 +4,11 @@ import './product_create.dart';
 import './product_list.dart';
 
 class ProductsAdminPage extends StatelessWidget {
+  final Function addProduct;
+  final Function deleteProduct;
+
+  ProductsAdminPage(this.addProduct, this.deleteProduct);
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -38,7 +43,7 @@ class ProductsAdminPage extends StatelessWidget {
           ),
         ),
         body: TabBarView(
-          children: <Widget>[ProductCreatePage(), ProductListPage()],
+          children: <Widget>[ProductCreatePage(addProduct), ProductListPage()],
         ),
       ),
     );
