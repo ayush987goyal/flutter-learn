44729aadedaccf8eb2e217d8cf3ee881215edb29
Ayush Goyal <ayush987goyal@gmail.com>
2018-07-22 18:52:59 +0530

    Add product model and refactor

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import './pages/auth.dart';
 import './pages/products.dart';
 import './pages/product.dart';
 import './pages/products_admin.dart';
+import './models/product.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,15 +16,15 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  List<Map<String, dynamic>> _products = [];
+  List<Product> _products = [];
 
-  void _addProduct(Map<String, dynamic> product) {
+  void _addProduct(Product product) {
     setState(() {
       _products.add(product);
     });
   }
 
-  void _updateProduct(int index, Map<String, dynamic> product) {
+  void _updateProduct(int index, Product product) {
     setState(() {
       _products[index] = product;
     });
@@ -61,10 +62,10 @@ class _MyAppState extends State<MyApp> {
           final int index = int.parse(pathElements[2]);
           return MaterialPageRoute<bool>(
             builder: (BuildContext context) => ProductPage(
-                  _products[index]['title'],
-                  _products[index]['image'],
-                  _products[index]['description'],
-                  _products[index]['price'],
+                  _products[index].title,
+                  _products[index].image,
+                  _products[index].description,
+                  _products[index].price,
                 ),
           );
         }
