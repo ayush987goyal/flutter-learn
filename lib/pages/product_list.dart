befb5293fd241f2c4c0a7ff0463845f148eb7b35
Ayush Goyal <ayush987goyal@gmail.com>
2018-07-22 18:32:52 +0530

    Add list item dismiss func

--- lib/pages/product_list.dart
+++ lib/pages/product_list.dart
@@ -5,38 +5,48 @@ import './product_edit.dart';
 class ProductListPage extends StatelessWidget {
   final List<Map<String, dynamic>> products;
   final Function updateProduct;
+  final Function deleteProduct;
 
-  ProductListPage(this.products, this.updateProduct);
+  ProductListPage(this.products, this.updateProduct, this.deleteProduct);
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (BuildContext context, int index) {
-        return Column(
-          children: <Widget>[
-            ListTile(
-              leading: CircleAvatar(
-                backgroundImage: AssetImage(products[index]['image']),
+        return Dismissible(
+          key: Key(products[index]['title']),
+          background: Container(color: Colors.red),
+          onDismissed: (DismissDirection direction) {
+            if (direction == DismissDirection.endToStart) {
+              deleteProduct(index);
+            }
+          },
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage(products[index]['image']),
+                ),
+                title: Text(products[index]['title']),
+                subtitle: Text('\$${products[index]['price']}'),
+                trailing: IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(builder: (BuildContext context) {
+                        return ProductEditPage(
+                          product: products[index],
+                          updateProduct: updateProduct,
+                          productIndex: index,
+                        );
+                      }),
+                    );
+                  },
+                ),
               ),
-              title: Text(products[index]['title']),
-              subtitle: Text('\$${products[index]['price']}'),
-              trailing: IconButton(
-                icon: Icon(Icons.edit),
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(builder: (BuildContext context) {
-                      return ProductEditPage(
-                        product: products[index],
-                        updateProduct: updateProduct,
-                        productIndex: index,
-                      );
-                    }),
-                  );
-                },
-              ),
-            ),
-            Divider()
-          ],
+              Divider()
+            ],
+          ),
         );
       },
       itemCount: products.length,
