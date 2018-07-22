d288c290437a38ffacba4c6fbc01af4105e16291
Ayush Goyal <ayush987goyal@gmail.com>
2018-07-22 13:28:57 +0530

    reusing create product and add update func

--- lib/pages/product_list.dart
+++ lib/pages/product_list.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 
+import './product_edit.dart';
+
 class ProductListPage extends StatelessWidget {
   final List<Map<String, dynamic>> products;
+  final Function updateProduct;
 
-  ProductListPage(this.products);
+  ProductListPage(this.products, this.updateProduct);
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +17,17 @@ class ProductListPage extends StatelessWidget {
           title: Text(products[index]['title']),
           trailing: IconButton(
             icon: Icon(Icons.edit),
-            onPressed: () {},
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (BuildContext context) {
+                  return ProductEditPage(
+                    product: products[index],
+                    updateProduct: updateProduct,
+                    productIndex: index,
+                  );
+                }),
+              );
+            },
           ),
         );
       },
